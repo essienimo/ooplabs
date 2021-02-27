1ce98c5b7187f27c3d9f7e61aafdeea64089f3ba
essienimo <79739543+essienimo@users.noreply.github.com>
2021-02-27 10:39:45 +0300

    Update index.java
    I have added a comment in the index.java file.

--- index.java
+++ index.java
@@ -1,7 +1,8 @@
 public class Index{
 	private String name;
 	private int age;
-
+	
+	//this is a parameterise constructor
 	public Index (String name, int age){
 		this.name = name;
 		this.age = age;
